2faa336c64b57a0fce5ffccba7eecd3e39d21385
ConDawg <31518714+ConDawg@users.noreply.github.com>
2017-09-19 12:46:48 -0600

    made some If statements that make it so I cannot  go into a negative number.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -25,7 +25,25 @@ public class MonsterController
 			System.out.println(currentMonster.getName() + " wants to know how many eyes you want to eat, please type in how many");
 			int consumed = myScanner.nextInt();
 			currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
-			System.out.println(currentMonster);
+			System.out.println(currentMonster);	
+			
+			System.out.println("How many arms would you like to consume?, I have " + currentMonster.getArmCount());
+			//consumed = myScanner.nextInt();
+			int armEat = myScanner.nextInt();
+			
+			if(armEat == 0)
+			{
+				System.out.println("Not hungry?  That sux bro...");
+			}
+			else if(armEat < 0)
+			{
+				System.out.println("Math is hard for you - it is not possible to eat a negative amount");
+			}
+			else
+			{
+				currentMonster.setArmCount(currentMonster.getArmCount() - armEat);
+				System.out.println("OK, now I have this many arms " + currentMonster.getArmCount());
+			}
 			
 			myScanner.close();
 		}
