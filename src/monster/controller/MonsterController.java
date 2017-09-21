7229de27899139ff11a523cf3012fec0376dd42c
ConDawg <31518714+ConDawg@users.noreply.github.com>
2017-09-21 13:44:21 -0600

    Cleaned up some stuff and played with it, trying to figure out how it works

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -38,31 +38,36 @@ public class MonsterController
 			
 			if(isValidInterger(response))
 			{
-			consumed = Integer.parseInt(response);
+				consumed = Integer.parseInt(response);
 			}	
 			
 //			int consumed = myScanner.nextInt();
 			currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
 			System.out.println(currentMonster);	
+//			System.out.println("How many arms are you gunna eat?");
+			response = popup.getResponse("Neat! how many arms are you gonna eat?");
 			{
 			//consumed = myScanner.nextInt();
-			int eyeEat = myScanner.nextInt();
+			int armEat = myScanner.nextInt();
 			
-			if(eyeEat == 0)
+			if(armEat == 0)
 			{
-				System.out.println("Freals?, I think the eyes are the best part!");
+				System.out.println("Freals?, I think the arms are the best part!");
 			}
-			else if(eyeEat < 0)
+			else if(armEat < 0)
 			{
-				System.out.println("Silly billy, you cant eat negative eyeholes!");
+				System.out.println("Silly billy, you cant eat negative arms!");
+				popup.displayText("Silly Billy, you cant eat negative arms!");
 			}
-			else if(eyeEat - currentMonster.getEyeCount() > 0)
+			else if(armEat - currentMonster.getEyeCount() > 0)
 			{
 				System.out.println("you are being a little too silly");
 			}
 			}
 //			System.out.println("How many arms would you like to consume?, I have " + currentMonster.getArmCount());
 			String solute = popup.getResponse(currentMonster.getName() + " wants to know how many arms you want to eat, please type how many");
+			
+			
 			
 			if(isValidInterger(solute));
 			{
@@ -74,26 +79,41 @@ public class MonsterController
 			
 			if(armEat == 0)
 			{
-				System.out.println("Not hungry?  That sux bro...");
+//				System.out.println("Not hungry?  That sux bro...");
+				popup.displayText("not hungry?  That sux bro...");
 			}
 			else if(armEat < 0)
 			{
-				System.out.println("Math is hard for you - it is not possible to eat a negative amount");
+//				System.out.println("Math is hard for you - it is not possible to eat a negative amount");
+				popup.displayText("Math is hard for you - it is not possible to eat a negative amount");
 			}
 			else if(armEat - currentMonster.getArmCount() > 0)
 			{
-				System.out.println("you are not allowed to eat more than exists on me rarXD");
+				System.out.println("you are not allowed to eat more than exists on me");
+				popup.displayText("you are not allowed to eat more than exists on me");
 			}
 			else
 			{
 				currentMonster.setArmCount(currentMonster.getArmCount() - armEat);
-				System.out.println("OK, now I have this many arms " + currentMonster.getArmCount());
+//				System.out.println("OK, now I have this many arms " + currentMonster.getArmCount());
+				popup.displayText("OK, now I have this many arms " + currentMonster.getArmCount());
 			}
 			
-			System.out.println("How many tentacles do you wanna eat? I have" + currentMonster.getTentacleAmount());
+			
+			
+//			System.out.println("How many tentacles do you wanna eat? I have" + currentMonster.getTentacleAmount());
+			popup.displayText("How many tentacles do you wanna eat? I have" + currentMonster.getTentacleAmount());
+			
 			double food = myScanner.nextDouble();
 			
+			String tentacleResponse = popup.getResponse("How many tentacles do you want to eat? I have" + currentMonster.getTentacleAmount());
+			if(isValidDouble(tentacleResponse))
+			{
+				food = Double.parseDouble(tentacleResponse);
+			}
+			
 			if(food == currentMonster.getTentacleAmount())
+			
 			{
 				System.out.println("wut the hek bro, u ate all my tenticles");
 			}
@@ -126,4 +146,41 @@ public class MonsterController
 			}
 			return valid;
 		}
+		
+		private boolean isValidDouble(String sampleDouble)
+		{
+			boolean valid = false;
+			
+			try
+			{
+				Double.parseDouble(sampleDouble);
+				valid = true;
+			}
+			catch(NumberFormatException error)
+			{
+				popup.displayText("You need to type in a double - " + sampleDouble + "is not a valid answer");
+			}
+			
+			return valid;
+		}	
+			
+		private boolean isValidBoolean(String sampleBoolean)
+		{
+			boolean valid = false;
+			
+			try
+			{
+				Boolean.parseBoolean(sampleBoolean);
+				valid = true;
+			}
+			catch(NumberFormatException error)
+			{
+				popup.displayText("Type in a boolean value" + sampleBoolean + " does not count.");
+					
+			}
+			
+			return valid;
+		}
+			
+		
 	}	
